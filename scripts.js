cda632293ef3da0545fd74c474eb5fecc7d9882f
Prisca <plp2112@columbia.edu>
2014-02-26 01:17:02 -0500

    Document ready is working

---
+++ scripts.js
@@ -0,0 +1,20 @@
+/**
+ * @author Prisca Pointdujour
+ */
+/*
+ * Project steps:
+ * 1. Set up document ready
+ * 2. Load Google charting package
+ * 3. Load data
+ * 4. Render chart
+*/
+
+function callback(){
+	console.log("page done");
+}
+
+/*document ready function */
+$(document).ready(callback)
+
+console.log("js loaded");
+ 
