8223b7c5a645e3079ccc3b89c2fd7e84a8ecfc3e
Prisca <plp2112@columbia.edu>
2014-02-27 01:07:47 -0500

    Data loaded

--- scripts.js
+++ scripts.js
@@ -9,12 +9,19 @@
  * 4. Render chart
 */
 
+function infoLoaded(UEMP270V){
+	console.log(UEMP270V);
+	
+}
+
 //defining my google.load callback
 function Pkgloaded(){
 	console.log("Google pkg loaded");
+	//setting up jQuery "get" call to load data
+	$.get("UEMP270V_data.json", infoLoaded, "json");
 }
 
-
+//defining document ready callback
 function Polo(){
 	console.log("page done");
 	//load Google charting package
