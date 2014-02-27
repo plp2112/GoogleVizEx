f05ce19965aeaf7f10e29f1ef7e08147579b4971
Prisca <plp2112@columbia.edu>
2014-02-27 03:06:42 -0500

    Line chart created

--- scripts.js
+++ scripts.js
@@ -17,6 +17,9 @@ function infoLoaded(UEMP270V){
 	var Containerlist = [];
 	//to create my visualization, I need to convert
 	//my JSON data to an array of arrays, using a for loop
+	//I need headers to be first
+	var myHeader = ["Date", "Value"];
+	Containerlist.push(myHeader);
 	
 	//specify starting point, ending point
 	for(var i=0; i<Observe.length; i++){
@@ -30,6 +33,17 @@ function infoLoaded(UEMP270V){
 	//at end of for loop, Containerlist should be populated with 
 	//array of arrays that is date and value of observations
 	console.log(Containerlist);
+	//feed data to visualization library
+	var DataTable = google.visualization.arrayToDataTable(Containerlist);
+	
+	//create options object to customize chart look
+	var options = {
+		title: "Unemployment: 1980 - 2013"
+	}
+	
+	//equivalent to jQuery $("#divName")
+	var chart = new google.visualization.LineChart(document.getElementById("EmptyDiv"));
+		chart.draw(DataTable, options);
 }
 
 //defining my google.load callback
